e03d6919bfde7884325ef49ab5e48c2d16acef0d
Nick Slusarczyk <axxessdenied@gmail.com>
2023-04-11 18:14:35 -0400

    glad

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "Glad"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/glad/gl.h",
+        "include/glad/vulkan.h",
+        "include/vk_platform.h",
+        "include/KHR/khrplatform.h",
+        "src/gl.c",
+        "src/vulkan.c"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
